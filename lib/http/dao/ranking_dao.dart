78409e1336208e5d54a0dea3f5b27a2cf92dbfd7
Jobs Sun <jobs@JobsdeMacBook-Pro.local>
2021-09-22 14:59:04 +0800

    排行榜

--- lib/http/dao/ranking_dao.dart
+++ lib/http/dao/ranking_dao.dart
@@ -4,10 +4,10 @@ import 'package:bili_app/model/ranking_mo.dart';
 
 class RankingDao {
   //https://api.devio.org/uapi/fa/ranking?sort=like&pageIndex=1&pageSize=40
-  static get(String sortKey, {int pageIndex = 1, int pageSize = 1}) async {
+  static get(String sort, {int pageIndex = 1, int pageSize = 1}) async {
     RankingRequest request = RankingRequest();
     request
-        .add("sort", sortKey)
+        .add("sort", sort)
         .add("pageIndex", pageIndex)
         .add("pageSize", pageSize);
     var result = await HiNet.getInstance().fire(request);
--- lib/page/ranking_tab_page.dart
+++ lib/page/ranking_tab_page.dart
@@ -1,3 +1,4 @@
+import 'package:bili_app/core/hi_base_tab_state.dart';
 import 'package:bili_app/http/core/hi_error.dart';
 import 'package:bili_app/http/dao/ranking_dao.dart';
 import 'package:bili_app/model/ranking_mo.dart';
@@ -9,102 +10,39 @@ import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class RankingTabPage extends StatefulWidget {
-  final String sortKey;
+  final String sort;
 
-  const RankingTabPage({Key? key, required this.sortKey}) : super(key: key);
+  const RankingTabPage({Key? key, required this.sort}) : super(key: key);
 
   @override
   _RankingTabPageState createState() => _RankingTabPageState();
 }
 
-class _RankingTabPageState extends State<RankingTabPage>
-    with AutomaticKeepAliveClientMixin {
-  List<VideoModel> videoList = [];
-  int pageIndex = 1;
-  bool _loading = false;
-  ScrollController _scrollController = ScrollController();
-
+class _RankingTabPageState
+    extends HiBaseTabState<RankingMo, VideoModel, RankingTabPage> {
   @override
-  void initState() {
-    super.initState();
-    _scrollController.addListener(() {
-      var dis = _scrollController.position.maxScrollExtent -
-          _scrollController.position.pixels;
-      print('dis:$dis');
-      //当距离底部不足300时加载更多
-      if (dis < 300 && !_loading) {
-        print('------_loadData---');
-        _loadData(loadMore: true);
-      }
-    });
-    _loadData();
-  }
-
-  @override
-  void dispose() {
-    _scrollController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    super.build(context);
-    return RefreshIndicator(
-      onRefresh: _loadData,
-      color: primary,
-      child: MediaQuery.removePadding(
-          removeTop: true,
-          context: context,
-          child: StaggeredGridView.countBuilder(
-              controller: _scrollController,
-              physics: const AlwaysScrollableScrollPhysics(),
-              padding: EdgeInsets.only(top: 10, left: 10, right: 10),
-              crossAxisCount: 1,
-              itemCount: videoList.length,
-              itemBuilder: (BuildContext context, int index) {
-                return VideoCard(videoMo: videoList[index]);
-              },
-              staggeredTileBuilder: (int index) {
-                return StaggeredTile.fit(1);
-              })),
-    );
-  }
-
-  Future<void> _loadData({loadMore = false}) async {
-    _loading = true;
-    if (!loadMore) {
-      pageIndex = 1;
-    }
-    var currentIndex = pageIndex + (loadMore ? 1 : 0);
-    print('Ranking:currentIndex:$currentIndex');
-    try {
-      RankingMo result = await RankingDao.get(widget.sortKey,
-          pageIndex: currentIndex, pageSize: 10);
-      setState(() {
-        if (loadMore) {
-          if (result.videoList.isNotEmpty) {
-            //合成一个新数组
-            videoList = [...videoList, ...result.videoList];
-            pageIndex++;
-          }
-        } else {
-          videoList = result.videoList;
-        }
+  get contentChild => StaggeredGridView.countBuilder(
+      controller: scrollController,
+      physics: const AlwaysScrollableScrollPhysics(),
+      padding: EdgeInsets.only(top: 10),
+      crossAxisCount: 1,
+      itemCount: dataList.length,
+      itemBuilder: (BuildContext context, int index) {
+        return VideoCard(videoMo: dataList[index]);
+      },
+      staggeredTileBuilder: (int index) {
+        return StaggeredTile.fit(1);
       });
-      Future.delayed(Duration(milliseconds: 1000), () {
-        _loading = false;
-      });
-    } on NeedAuth catch (e) {
-      _loading = false;
-      print(e);
-      showWarnToast(e.message);
-    } on HiNetError catch (e) {
-      _loading = false;
-      print(e);
-      showWarnToast(e.message);
-    }
+
+  @override
+  Future<RankingMo> getData(int pageIndex) async {
+    RankingMo result =
+        await RankingDao.get(widget.sort, pageIndex: pageIndex, pageSize: 1);
+    return result;
   }
 
   @override
-  bool get wantKeepAlive => true;
+  List<VideoModel> parseList(RankingMo result) {
+    return result.list;
+  }
 }
